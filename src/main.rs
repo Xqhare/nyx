0b55ab4dc1ac49801d1461b40af83b222e39689f
Xqhare <xqhare@web.de>
2024-01-15 19:56:49 +0100

    added constants Appname, version and authors

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use gui::start_nyx;
 mod gui;
 mod comp;
 
+const APPNAME: &str = env!("CARGO_PKG_NAME");
+const APPVERSION: &str = env!("CARGO_PKG_VERSION");
+const APPAUTHORS: &str = env!("CARGO_PKG_AUTHORS");
+
 fn main() {
     start_nyx();
 }
