4f695de19355aad66b957ca9efc34caff9dfd9d9
Xqhare <xqhare@web.de>
2024-01-25 00:22:06 +0100

    using more appstate data

--- src/comp/cpu.rs
+++ src/comp/cpu.rs
@@ -6,10 +6,10 @@ pub struct CpuData {
 }
 
 impl CpuData {
-    pub fn new(data: Vec<f64>) -> Self {
+    pub fn new(data: Vec<f64>, num_cores: u8) -> Self {
         let core_data = {
             let mut out: Vec<VecDeque<f64>> = Default::default();
-            for _n in 1..=12 {
+            for _n in 1..=num_cores {
                 let queue = VecDeque::from(data.clone());
                 out.push(queue);
             }
