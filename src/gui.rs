9d28c1e8e11ffd88e85ecd6e6aff989834ffb615
Xqhare <xqhare@web.de>
2024-01-24 23:22:18 +0100

    added next update check, implemented drawing of all components except gpu, added placeholder components subpages, slight rework of appstate from testdata generation

--- src/gui.rs
+++ src/gui.rs
@@ -1,32 +1,54 @@
-
+use chrono::{Utc, Duration, SecondsFormat};
 use eframe::{*, epaint::{Vec2, Color32}, egui::{CentralPanel, Ui, ScrollArea, Grid}};
 use egui_plot::{BarChart, Bar, Plot};
 
-use crate::{APPNAME, APPVERSION, APPAUTHORS};
+use crate::time_now_rfc3339zulu;
+
+use crate::{APPNAME, APPVERSION, APPAUTHORS, comp::{network::Network, disc::Disk, cpu::CpuData}};
+
+const DATAUPDATEINTERVAL: i64 = 1;
+
 
 struct Nyx {
+    // AppData
+    next_data_update: String,
+    // Data
     test_data: Vec<f64>,
     num_cores: u8,
-    show_help: bool,
+    networks: Vec<Network>,
+    disks: Vec<Disk>,
+    cpu_data: CpuData,
+
+    // Drawing booleans
     show_landing_page: bool,
+    show_help: bool,
     show_cpu_page: bool,
     show_ram_page: bool,
+    show_gpu_page: bool,
+    show_disk_page: bool,
+    show_temperature_page: bool,
+    
+    // Settings
     display_size: Vec2,
 }
 
 impl Default for Nyx {
 
     fn default() -> Self {
-        let test_data = vec![10.4, 56.0, 15.4, 68.7, 91.25, 41.2, 56.47, 41.54, 10.4, 56.0, 15.4, 68.7, 91.25, 41.2, 56.47, 41.54, 10.3, 1.0, 2.2, 4.3, 2.6, 3.8, 10.4, 56.0, 15.4, 68.7, 91.25, 41.2, 56.47, 41.54, 10.4, 56.0, 15.4, 68.7, 91.25, 41.2, 56.47, 41.54, 10.3, 1.0, 2.2, 4.3, 2.6, 3.8, 10.4, 56.0, 15.4, 68.7, 91.25, 41.2, 56.47, 41.54, 10.4, 56.0, 15.4, 68.7, 91.25, 41.2, 56.47, 41.54,];
+        let test_data: Vec<f64> = vec![10.4, 56.0, 15.4, 68.7, 91.25, 41.2, 56.47, 41.54, 10.4, 56.0, 15.4, 68.7, 91.25, 41.2, 56.47, 41.54, 10.3, 1.0, 2.2, 4.3, 2.6, 3.8, 10.4, 56.0, 15.4, 68.7, 91.25, 41.2, 56.47, 41.54, 10.4, 56.0, 15.4, 68.7, 91.25, 41.2, 56.47, 41.54, 10.3, 1.0, 2.2, 4.3, 2.6, 3.8, 10.4, 56.0, 15.4, 68.7, 91.25, 41.2, 56.47, 41.54, 10.4, 56.0, 15.4, 68.7, 91.25, 41.2, 56.47, 41.54,];
         let num_cores: u8 = 12;
+        let networks = vec![Network::new("First".to_string()), Network::new("Second".to_string())];
+        let disks = vec![Disk::new("One".to_string()), Disk::new("Two".to_string())];
         // TODO Put display_size into settings
         let display_size: Vec2 = Vec2 { x: 1200.0, y: 900.0 };
+        let next_data_update = next_update_time();
+        let cpu_data = CpuData::new(test_data.clone());
         Nyx { 
-            test_data, num_cores,  display_size,
+            test_data, num_cores,  display_size, networks, disks, next_data_update, cpu_data,
             // default true
             show_landing_page: true,
             // default false
-            show_cpu_page: false, show_ram_page: false, show_help: false,
+            show_cpu_page: false, show_ram_page: false, show_help: false, show_gpu_page: false, show_disk_page: false, show_temperature_page: false,
         }
     }
 
@@ -37,6 +59,9 @@ impl App for Nyx {
     fn update(&mut self, ctx: &egui::Context, frame: &mut Frame) {
         CentralPanel::default()
             .show(ctx, |ui: &mut Ui| {
+                if time_now_rfc3339zulu(SecondsFormat::Secs) >= self.next_data_update {
+                    self.next_data_update = next_update_time();
+                }
                 self.draw_main_menu(ui);
                 if self.show_help {
                 }
@@ -66,6 +91,9 @@ impl Nyx {
         self.show_help = false;
         self.show_cpu_page = false;
         self.show_ram_page = false;
+        self.show_gpu_page = false;
+        self.show_disk_page = false;
+        self.show_temperature_page = false;
     }
 
     fn draw_main_menu(&mut self, ui: &mut Ui) {
@@ -79,6 +107,9 @@ impl Nyx {
                 }
                 if ui.button("About").clicked() {
                     println!("About click");
+                }
+                if ui.button("Eris").clicked() {
+                    println!("Eris click");
                 }
             });
             if ui.button("CPU").clicked() {
@@ -113,11 +144,20 @@ impl Nyx {
     }
 
     fn draw_landing_page(&mut self, ui: &mut Ui) {
+        ui.heading("CPU");
         self.grid_cpu_landing_page(ui);
+        ui.separator();
+        ui.heading("GPU");
         self.grid_gpu_landing_page();
+        ui.separator();
+        ui.heading("RAM");
         self.grid_ram_landing_page(ui);
-        self.grid_discs_landing_page();
-        self.gird_networks_landing_page();
+        ui.separator();
+        ui.heading("Disks");
+        self.grid_discs_landing_page(ui);
+        ui.separator();
+        ui.heading("Networks");
+        self.gird_networks_landing_page(ui);
     }
 
     fn grid_cpu_landing_page(&mut self, ui: &mut Ui) {
@@ -197,7 +237,7 @@ impl Nyx {
         });
     }
 
-    fn draw_ram_usage(&self, ui: &mut Ui, mem_swap: &str) {
+    fn draw_ram_usage(&mut self, ui: &mut Ui, mem_swap: &str) {
         ui.vertical_centered_justified(|ui: &mut Ui| {
             let data = match mem_swap {
                 "ram" => &self.test_data,
@@ -220,21 +260,102 @@ impl Nyx {
                 .show(ui, |plot_ui| plot_ui.bar_chart(chart));
             // If code below is changed, change it to the same in `draw_cpu_avg_load`
             if ram_plot.response.clicked(){
-                println!("RAM CLICKED")
+                self.ram_clicked();
             }
         });
     }
 
-    fn grid_discs_landing_page(&self) {
-        // WIP
+    fn ram_clicked(&mut self) {
+        println!("RAM MENU CLICKED");
+        self.clear_screen();
+        self.show_ram_page = true;
     }
 
-    fn gird_networks_landing_page(&self) {
-        // WIP
+    fn grid_discs_landing_page(&mut self, ui: &mut Ui) {
+        ui.add(|ui: &mut Ui| {
+            Grid::new("Disks").striped(true).min_col_width((self.display_size.x / 2.0) - 50.0).num_columns(self.disks.len()).show(ui, |ui: &mut Ui| {
+                for disk in &self.disks {
+                    ui.label(disk.name.clone());
+                }
+                ui.end_row();
+                for desk in self.disks.clone() {
+                    self.draw_disk_usage(ui, desk.clone());
+                }
+            }).response
+        });    
+    }
+
+    fn draw_disk_usage(&mut self, ui: &mut Ui, disk: Disk) {
+        ui.vertical_centered_justified(|ui: &mut Ui| {
+            let chart = BarChart::new(disk.data.iter().enumerate().map(|x| {
+                (x.1, x.0 as f64)
+            }).map(|(x, y)| Bar::new(y, *x).width(1.0)).collect()
+            ).color(Color32::GOLD);
+
+            let ram_plot = Plot::new(format!("{} Usage", disk.name).as_str())
+                .show_axes(false)
+                .y_axis_width(3)
+                .allow_zoom(false)
+                .allow_drag(false)
+                .allow_scroll(false)
+                .allow_boxed_zoom(false)
+                .include_y(100.0)
+                .set_margin_fraction(Vec2 { x: 0.0, y: 0.0 })
+                .show(ui, |plot_ui| plot_ui.bar_chart(chart));
+            if ram_plot.response.clicked(){
+                self.ram_clicked();
+            }
+        });
+    }
+
+    fn gird_networks_landing_page(&mut self, ui: &mut Ui) {
+        ui.add(|ui: &mut Ui| {
+            Grid::new("Networks").striped(true).min_col_width((self.display_size.x / 2.0) - 50.0).num_columns(self.networks.len()).show(ui, |ui: &mut Ui| {
+                for network in &self.networks {
+                    ui.label(network.name.clone());
+                }
+                ui.end_row();
+                for network in self.networks.clone() {
+                    self.draw_network_usage(ui, network.clone());
+                }
+            }).response
+        });
+    }
+
+    fn draw_network_usage(&mut self, ui: &mut Ui, network: Network) {
+        ui.vertical_centered_justified(|ui: &mut Ui| {
+            let chart = BarChart::new(network.data.iter().enumerate().map(|x| {
+                (x.1, x.0 as f64)
+            }).map(|(x, y)| Bar::new(y, *x).width(1.0)).collect()
+            ).color(Color32::GOLD);
+
+            let ram_plot = Plot::new(format!("{} Usage", network.name).as_str())
+                .show_axes(false)
+                .y_axis_width(3)
+                .allow_zoom(false)
+                .allow_drag(false)
+                .allow_scroll(false)
+                .allow_boxed_zoom(false)
+                .include_y(100.0)
+                .set_margin_fraction(Vec2 { x: 0.0, y: 0.0 })
+                .show(ui, |plot_ui| plot_ui.bar_chart(chart));
+            if ram_plot.response.clicked(){
+                self.ram_clicked();
+            }
+        });
     }
 
     fn grid_gpu_landing_page(&self) {
         // WIP
+    }
+}
+
+fn next_update_time() -> String {
+    let now = Utc::now().checked_add_signed(Duration::seconds(DATAUPDATEINTERVAL));
+    if now.is_some() {
+        return now.unwrap().to_rfc3339_opts(chrono::SecondsFormat::Secs, true);
+    } else {
+        return Utc::now().to_rfc3339_opts(SecondsFormat::Secs, true);
     }
 }
 
