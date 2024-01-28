1445f5837d910840a630479db92618df7456da2a
Xqhare <xqhare@web.de>
2024-01-28 21:05:09 +0100

    added get_cpu_data as it is a standalone utility function

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,8 @@
+use std::thread;
+
 use chrono::{Utc, Duration, SecondsFormat};
 use rand::{prelude::SliceRandom, thread_rng};
+use sysinfo::{System, RefreshKind, CpuRefreshKind, MINIMUM_CPU_UPDATE_INTERVAL};
 
 pub fn time_now_rfc3339zulu(seconds_format: SecondsFormat) -> String {
     Utc::now().to_rfc3339_opts(seconds_format, true)
@@ -21,3 +24,28 @@ pub fn next_update_time(interval: Duration) -> String {
         return Utc::now().to_rfc3339_opts(SecondsFormat::Secs, true);
     }
 }
+
+pub fn get_cpu_data() -> (Vec<f64>, f64) {
+        let mut sys = System::new_with_specifics(RefreshKind::new().with_cpu(CpuRefreshKind::everything().without_frequency()));
+        sys.refresh_cpu_usage();
+        // because of some funky shit sysinfo does internally, it is adviced to update twice for consistent data.
+        thread::sleep(MINIMUM_CPU_UPDATE_INTERVAL);
+        sys.refresh_cpu_usage();
+        // Real work
+        let mut tmp_store: Vec<f64> = Default::default();
+        for cpu in sys.cpus() {
+            tmp_store.push(cpu.cpu_usage() as f64);
+        }
+        // reason for own ang_load calc: Ref D1
+        let avg = {
+            let sum = {
+                let mut out: f64 = 0.0;
+                for n in &tmp_store {
+                    out = out + n;
+                }
+                out
+            };
+            sum / tmp_store.len() as f64
+        };
+        return (tmp_store, avg);
+    }
