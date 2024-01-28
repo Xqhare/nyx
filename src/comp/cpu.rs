ea88f0057a0c8f9489daa1e06c3279a816460bb4
Xqhare <xqhare@web.de>
2024-01-28 16:03:42 +0100

    Wrapped vectors and queues in Arc

--- src/comp/cpu.rs
+++ src/comp/cpu.rs
@@ -1,21 +1,21 @@
-use std::collections::VecDeque;
+use std::{collections::VecDeque, sync::Arc};
 
 pub struct CpuData {
-    pub core_data: Vec<VecDeque<f64>>,
-    pub avg_load: VecDeque<f64>,
+    pub core_data: Arc<Vec<Arc<VecDeque<f64>>>>,
+    pub avg_load: Arc<VecDeque<f64>>,
 }
 
 impl CpuData {
     pub fn new(data: Vec<f64>, num_cores: u8) -> Self {
         let core_data = {
-            let mut out: Vec<VecDeque<f64>> = Default::default();
+            let mut out: Vec<Arc<VecDeque<f64>>> = Default::default();
             for _n in 1..=num_cores {
                 let queue = VecDeque::from(data.clone());
-                out.push(queue);
+                out.push(queue.into());
             }
             out
         };
         let avg_load = VecDeque::from(data);
-        CpuData { core_data, avg_load }
+        CpuData { core_data: core_data.into(), avg_load: avg_load.into() }
     }
 }
