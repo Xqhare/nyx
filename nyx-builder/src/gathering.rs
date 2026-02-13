86a9990cb95b9dae298662d068e39d99f9d4fd36
Xqhare <xqhare@web.de>
2026-02-13 22:11:47 +0100

    feat(nyx-builder) basic backend implemented, ready for frontend

---
+++ nyx-builder/src/gathering.rs
@@ -0,0 +1,68 @@
+use std::time::Instant;
+
+use hermes::Hermes;
+use athena::{Object, XffValue};
+use nyx_backend::{error::NyxResult, gathering::{df_gatherer, docker_gatherer, free_gatherer, ps_gatherer, shamash_gatherer, uptime_gatherer}};
+
+use crate::{GATHER_INTERVAL, GATHER_SERVER};
+
+/// All setup code that can panic or fail
+pub fn setup_gathering_server() {
+    std::thread::spawn(move || {
+        let con = Hermes::new(GATHER_SERVER).expect("Failed to create Hermes Server");
+        let mut running = true;
+        let mut last_run = Instant::now();
+        while running {
+            if con.is_request_ready() {
+                let request = con.await_request();
+                match request {
+                    Ok(request) => {
+                        if request.is_null() {
+                            running = false;
+                        }
+                    },
+                    Err(err) => {
+                        let err = XffValue::from(format!("Failed to get request: {:?}", err));
+                        if let Err(err) = con.put_error(err) {panic!("{:?}", err)};
+                    }
+                }
+            }
+
+            if !last_run.elapsed().as_millis() > GATHER_INTERVAL {
+                continue
+            }
+            last_run = Instant::now();
+
+            match gather() {
+                Ok(value) => {
+                    let mut value = value.into_object().expect("Failed to convert to object");
+                    value.insert("time", XffValue::from(last_run.elapsed().as_millis().to_string()));
+                    if let Err(err) = con.respond(value.into()) {panic!("{:?}", err)};
+                },
+                Err(err) => {
+                    let err = XffValue::from(format!("Failed to gather: {:?}", err));
+                    if let Err(err) = con.put_error(err) {panic!("{:?}", err)};
+                }
+            }
+
+        }
+    });
+}
+
+fn gather() -> NyxResult<XffValue> {
+    let df_gathered = df_gatherer()?;
+    let docker_gathered = docker_gatherer()?;
+    let free_gathered = free_gatherer()?;
+    let ps_gathered = ps_gatherer()?;
+    let uptime_gathered = uptime_gatherer()?;
+    let shamash_gathered = shamash_gatherer()?;
+
+    let mut obj = Object::new();
+    obj.insert("df", df_gathered);
+    obj.insert("docker", docker_gathered);
+    obj.insert("free", free_gathered);
+    obj.insert("ps", ps_gathered);
+    obj.insert("uptime", uptime_gathered);
+    obj.insert("shamash", shamash_gathered);
+    Ok(obj.into())
+}
--- nyx-builder/src/main.rs
+++ nyx-builder/src/main.rs
@@ -1,73 +1,38 @@
-use std::time::Instant;
-
+use athena::XffValue;
 use hermes::Hermes;
-use athena::{XffValue, Object};
-use nyx_backend::{error::NyxResult, gathering::{df_gatherer, docker_gatherer, free_gatherer, ps_gatherer, shamash_gatherer, uptime_gatherer}};
+use talos::Talos;
+
+use crate::gathering::setup_gathering_server;
+
+mod gathering;
+
+const GATHER_SERVER: &str = ".nxy_data/gathering";
+const GATHER_INTERVAL: u128 = 750;
 
 fn main() {
     // Space for future startup code
     
+    setup_gathering_server();
 
-}
-
-/// All setup code that can panic or fail
-fn setup_gathering_server() {
-    std::thread::spawn(move || {
-        let con = Hermes::new(".nxy_data/gathering").expect("Failed to create Hermes Server");
-        let mut running = true;
-        let mut last_run = Instant::now();
-        while running {
-            if con.is_request_ready() {
-                let request = con.await_request();
-                match request {
-                    Ok(request) => {
-                        if request.is_null() {
-                            running = false;
-                        }
-                    },
-                    Err(err) => {
-                        let err = XffValue::from(format!("Failed to get request: {:?}", err));
-                        if let Err(err) = con.put_error(err) {panic!("{:?}", err)};
-                    }
-                }
-            }
-
-            if !last_run.elapsed().as_millis() > 750 {
-                continue
-            }
-            last_run = Instant::now();
-
-            match gather() {
-                Ok(value) => {
-                    let mut value = value.into_object().expect("Failed to convert to object");
-                    value.insert("time", last_run.elapsed().as_millis());
-                    if let Err(err) = con.respond(value.into()) {panic!("{:?}", err)};
+    let mut run = true;
+    let mut con = Hermes::new(GATHER_SERVER).expect("Failed to create Hermes Server");
+    let mut state: XffValue = XffValue::Null;
+    let mut talos = Talos::builder().build().expect("Failed to create Talos");
+    while run {
+        if con.is_response_ready() {
+            let response = con.await_response();
+            match response {
+                Ok(response) => {
+                    state = response;
                 },
                 Err(err) => {
-                    let err = XffValue::from(format!("Failed to gather: {:?}", err));
-                    if let Err(err) = con.put_error(err) {panic!("{:?}", err)};
+                    // TODO: Handle error
+                    println!("Failed to get response: {:?}", err);
+                    run = false;
                 }
             }
-
         }
-    });
+
+    }
 }
 
-fn gather() -> NyxResult<XffValue> {
-
-    let df_gathered = df_gatherer()?;
-    let docker_gathered = docker_gatherer()?;
-    let free_gathered = free_gatherer()?;
-    let ps_gathered = ps_gatherer()?;
-    let uptime_gathered = uptime_gatherer()?;
-    let shamash_gathered = shamash_gatherer()?;
-
-    let mut obj = Object::new();
-    obj.insert("df", df_gathered);
-    obj.insert("docker", docker_gathered);
-    obj.insert("free", free_gathered);
-    obj.insert("ps", ps_gathered);
-    obj.insert("uptime", uptime_gathered);
-    obj.insert("shamash", shamash_gathered);
-    Ok(obj.into())
-}
