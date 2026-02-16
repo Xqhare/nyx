1d4a60a6dde0aff6282ddd35da61f50f01f9a4b1
Xqhare <xqhare@web.de>
2026-02-16 23:34:34 +0100

    add(nyx-builder): wait for state on first run

--- nyx-builder/src/main.rs
+++ nyx-builder/src/main.rs
@@ -1,5 +1,6 @@
 use athena::XffValue;
 use hermes::Hermes;
+use nyx_tui::draw_state;
 use talos::Talos;
 
 use crate::gathering::setup_gathering_server;
@@ -18,7 +19,22 @@ fn main() {
     let mut con = Hermes::new(GATHER_SERVER).expect("Failed to create Hermes Server");
     let mut state: XffValue = XffValue::Null;
     let mut talos = Talos::builder().build().expect("Failed to create Talos");
+    let mut first_iter = true;
     while run {
+        if first_iter {
+            let response = con.await_response();
+            match response {
+                Ok(response) => {
+                    state = response;
+                },
+                Err(err) => {
+                    // TODO: Handle error
+                    println!("Failed to get response: {:?}", err);
+                    run = false;
+                }
+            }
+            first_iter = false;
+        }
         if con.is_response_ready() {
             let response = con.await_response();
             match response {
@@ -32,7 +48,13 @@ fn main() {
                 }
             }
         }
-
+        match draw_state(state.clone(), &mut talos) {
+            Some(state) => {
+                if state == XffValue::Null {
+                } 
+            }
+            None => {}
+        }
     }
 }
 
